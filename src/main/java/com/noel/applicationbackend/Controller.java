da510562b0728dc0d2124202c1dcae59f47e6dcc
Noel Vock <noel1uk@gmail.com>
2019-02-27 07:32:30 +0000

    add test to ensure endpoint functions

---
+++ src/main/java/com/noel/applicationbackend/Controller.java
@@ -0,0 +1,9 @@
+package com.noel.applicationbackend;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+}
