9d5060c62e5b546f8b1c96a64d0d45a32f1400ae
Noel Vock <noel1uk@gmail.com>
2019-02-28 21:44:56 +0000

    add response entity with headers, body and status

--- src/main/java/com/noel/applicationbackend/Controller.java
+++ src/main/java/com/noel/applicationbackend/Controller.java
@@ -1,5 +1,8 @@
 package com.noel.applicationbackend;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
 
     @RequestMapping("/")
-    public String greeting() {
-        return "{id: 123, content: \"Hello\", city: \"New York\"}";
+    public ResponseEntity<String> greeting() {
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("Access-Control-Allow-Origin", "http://localhost:4200");
+        return new ResponseEntity<>(
+                "{\"id\": 123, \"content\": \"Hello\", \"city\": \"New York\"}",
+                responseHeaders,
+                HttpStatus.OK);
     }
 }
